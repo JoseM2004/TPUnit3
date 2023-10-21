32a7107ecf021d8527fee33d5ed387b340932506
JoseM2004 <jmmonsalvemarin00@gmail.com>
2023-10-20 23:15:23 -0500

    Reestructuracion del codigo en las clases CalculaPerimetroRectangulo1 y Facturaciom1 usando clean code

--- src/main/java/org/example/ejercicios/CalculaPerimetroRectangulo1.java
+++ src/main/java/org/example/ejercicios/CalculaPerimetroRectangulo1.java
@@ -1,16 +1,18 @@
 package org.example.ejercicios;
 
 public class CalculaPerimetroRectangulo1 {
+	record Rectangulo(double base, double altura) {}
 	public static void main(String[] args) {
-		Rectangulo rectangulo = new Rectangulo(4.0, 6.0, 4.0, 6.0);
-		double perimetro = 0;
-		double l1 = rectangulo.lado1();
-		double l2 = rectangulo.lado2();
-		double l3 = rectangulo.lado3();
-		double l4 = rectangulo.lado4();
-		perimetro = l1 + l2 + l3 + l4;
+		Rectangulo rectangulo = new Rectangulo(4.0, 6.0);
+		double perimetro;
+		perimetro = calcularPerimetroRectangulo(rectangulo);
 		System.out.println("El perímetro del rectángulo es: " + perimetro);
 	}
-
-	record Rectangulo(double lado1, double lado2, double lado3, double lado4) {}
+	static double calcularPerimetroRectangulo(Rectangulo rectangulo) {
+		double base, altura;
+		base = rectangulo.base();
+		altura = rectangulo.altura();
+		return (base + altura)*2;
+	}
 }
+
--- src/main/java/org/example/ejercicios/Facturaciom1.java
+++ src/main/java/org/example/ejercicios/Facturaciom1.java
@@ -1,31 +1,30 @@
 package org.example.ejercicios;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class Facturaciom1 {
-	record Factura(String cliente, String fechafactura, List<Double> precioProducto) {
+	record Factura(String cliente, LocalDate fechaFactura, List<Double> precioProducto) {
 	}
 
 	public static void main(String[] args) {
-		Factura factura1 = new Factura("Maria Sanabria", "Julio 1, 1999", List.of(4500D, 5000D, 2500D));
-		Factura factura2 = new Factura("Juan Perez", "Agosto 15, 2000", List.of(3500D, 6000D, 2800D));
+		Factura factura1 = new Factura("Maria Sanabria", LocalDate.parse("01/07/1999", DateTimeFormatter.ofPattern("dd/MM/uuuu")), List.of(4500D, 5000D, 2500D));
+		Factura factura2 = new Factura("Juan Perez", LocalDate.parse("15/08/2000", DateTimeFormatter.ofPattern("dd/MM/uuuu")), List.of(3500D, 6000D, 2800D));
+		double totalValorFactura1 , totalValorFactura2;
 
-		double totalValorFact1 = 0;
-		for (Double precio : factura1.precioProducto) {
-			totalValorFact1 += precio;
+		totalValorFactura1 = calcularFactura(factura1);
+		totalValorFactura2 = calcularFactura(factura2);
+		System.out.println("Factura 1 \nFecha: "+factura1.fechaFactura()+ "\nCliente: "+ factura1.cliente()+"\nTotal: "+ totalValorFactura1);
+		System.out.println("\nFactura 2 \nFecha: "+factura1.fechaFactura()+ "\nCliente: "+ factura2.cliente()+"\nTotal: "+ totalValorFactura2);
+
+	}
+	static double calcularFactura(Factura factura) {
+		double totalValorFactura = 0D;
+		for (Double precio : factura.precioProducto) {
+			totalValorFactura += precio;
 		}
-		System.out.println("Factura 1" );
-		System.out.println("Cliente: " + factura1.cliente());
-		System.out.println("Total: " + totalValorFact1);
-
-
-		double totalValorFact2 = 0;
-		for (Double precio : factura2.precioProducto) {
-			totalValorFact2 += precio;
-		}
-		System.out.println("Factura 2" );
-		System.out.println("Cliente: " + factura2.cliente());
-		System.out.println("Total: " + totalValorFact2);
+		return totalValorFactura;
 	}
 
 }
